11ca9e8afd3860c0c6ca8205685f1099ec71ca08
Navleen <navleen95@yahoo.ca>
2015-09-20 04:33:01 -0400

    Updated the AppEngine

--- App Engine/index.php
+++ App Engine/index.php
@@ -1,16 +1,29 @@
 <?php
+//http://gcp-hackthenorth-3212.appspot.com/
+
 session_start();
 
 include_once("shoppinglist.php");
 
 $ShoppingList = new ShoppingList();	
 
-if($_GET["import"] == true && $_POST["file"]){
-	$ShoppingList->setCSV($_POST["file"]);	
+if($_GET["import"] == true){
+	$ShoppingList->setCSV('food.csv');	
 	$ShoppingList->InsertStartingInfo();
-}else if($_GET["item"] == true){
-	$ShoppingList->SelectItems();
-}else if($_GET["list"] == true && $_POST["list"]){
-	$ShoppingList->SelectShoppingList(json_decode($_POST["list"]));
 }
+
+if($_GET["item"] == true){
+	echo ($ShoppingList->SelectItems());
+}else if($_GET["list"] == true){
+	echo $ShoppingList->getStoreTotals();
+}else if($_GET["final"]){
+	echo $ShoppingList->getFinalDisplay();
+}else if($_POST["list"]){
+	$ShoppingList->setStoreTotals(json_decode($_POST["list"]));
+}else if($_POST["store"] && $_POST["finallist"]){
+	$ShoppingList->setFinalDisplay(json_decode($_POST["store"]), json_decode($_POST["finallist"]));
+
+}
+
+
 ?>
--- App Engine/shoppinglist.php
+++ App Engine/shoppinglist.php
@@ -4,11 +4,16 @@ session_start();
 class ShoppingList{
 
 	private $_csvFile;
+	private $_arrStoreTotalInfo;
+	private $_arrFinalDisplay;
+	// private array(
+	// 	"strStoreName" => array("strStoreName"=>,"strLocation"=>"put location here","totalPrice"=>"put price variable here"),
+	// 	);
 
 	private function SQLConnect(){
 		$Connenct = mysql_connect(
-			':/cloudsql/gcp-hackthenorth-3212:shoppinglist',
-			//'173.194.106.13:3306',
+			//':/cloudsql/gcp-hackthenorth-3212:shoppinglist',
+			'173.194.106.13:3306',
 			'hackthenorth',
 			'hackthenorth'
 		);	
@@ -29,7 +34,7 @@ class ShoppingList{
 		foreach($csv as $row){
 			if($row[2]){
 				$strSQL = "INSERT INTO dbShoppingList.tblItems 
-							SET strItemName = '". $row[2]."'";
+							SET strItemName = '". $row[1]."'";
 				try{
 					mysql_query($strSQL);
 				}catch(Exception $e){
@@ -43,12 +48,13 @@ class ShoppingList{
 		$csv = $this->readCSV();
 		$this->SQLConnect();
 		foreach($csv as $row){
-			if($row[2]){
+			if($row[1]){
 				$strSQL = "INSERT INTO dbShoppingList.tblShoppingList
-							SET strStoreName = '". $row[1] ."',
-								intItemID = (SELECT intItemID FROM dbShoppingList.tblItems WHERE strItemName = '". $row[2] ."'),
-								strLocation = '". $row[3] ."',
-								fltPrice = '". $row[4]."'";
+							SET strStoreName = '". $row[0] ."',
+								intItemID = (SELECT intItemID FROM dbShoppingList.tblItems WHERE strItemName = '". $row[1] ."'),
+								strLocation = '". $row[2] ."',
+								douPrice = '". $row[3]."',
+								strSymbol = '". $row[4]."'";
 				try{
 					mysql_query($strSQL);
 				}catch(Exception $e){
@@ -71,14 +77,11 @@ class ShoppingList{
 		return json_encode($arrResult);
 	}
 
-	function SelectShoppingList($arrList){
+	private function SelectStoreName(){
 		$this->SQLConnect();
-		$strSQL = "SELECT strStoreName, strItemName, fltPrice, strLocation
+		$strSQL = "SELECT strStoreName
 					FROM dbShoppingList.tblShoppingList
-					LEFT JOIN dbShoppingList.tblItems
-					USING (intItemID) 
-					WHERE strItemName IN (". $arrList .")";		
-
+					GROUP BY strStoreName";
 		$result = mysql_query($strSQL);
 		$arrResult = array();
 		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
@@ -87,14 +90,81 @@ class ShoppingList{
 		return $arrResult;
 	}
 
+	function setStoreTotals($arrList){
+		$this->SQLConnect();
+		$strSQL = "SELECT strStoreName strItemName, SUM(douPrice), strLocation, strSymbol
+					FROM dbShoppingList.tblShoppingList
+					LEFT JOIN dbShoppingList.tblItems
+					USING (intItemID) 
+					WHERE strItemName IN (". $arrList .")
+					GROUP BY strStoreName";		
+
+		$result = mysql_query($strSQL);
+		$arrResult = array();
+		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+		    $arrResult[] = $row;
+		}
+		$this->_arrStoreTotalInfo = $arrResult;
+	}
+
+	function getStoreTotals(){
+		return json_encode($this->_arrStoreTotalInfo);
+	}
+
+	/*private function SelectShoppingList($arrList, $strStoreName){
+		$this->SQLConnect();
+		$strSQL = "SELECT strItemName, SUM(intPrice), strLocation, strSymbol
+					FROM dbShoppingList.tblShoppingList
+					LEFT JOIN dbShoppingList.tblItems
+					USING (intItemID) 
+					WHERE strItemName IN (". $arrList .")
+					AND strStoreName = ". $strStoreName;		
+
+		$result = mysql_query($strSQL);
+		$arrResult = array();
+		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+		    $arrResult[] = $row;
+		}
+		return $arrResult;
+	}*/
+
 	function InsertStartingInfo(){
 		$this->InsertItems();
 		$this->InsertShoppingList();
 	}
 
 	function setCSV($csvFile){
-		$this->_csvFile = "../CSV/".$csvFile;
+		$this->_csvFile = $csvFile;
 	}
+
+	function setFinalDisplay($strStoreName, $arrFinalList){
+		$this->SQLConnect();
+		$strSQL = "SELECT strItemName, douPrice, strSymbol
+					FROM dbShoppingList.tblShoppingList
+					LEFT JOIN dbShoppingList.tblItems
+					USING (intItemID) 
+					WHERE strItemName IN (". $arrFinalList .")
+					AND strStoreName = '".$strStoreName."'";		
+
+		$result = mysql_query($strSQL);
+		$arrResult = array();
+		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+		    $arrResult[] = $row;
+		}
+		$this->_arrFinalDisplay = $arrResult;		
+	}
+
+	function getFinalDisplay(){
+		return json_encode($this->_arrFinalDisplay);
+	}
+
+	/*function setStoreTotals($arrList){
+		$arrStoreName = $this->SelectStoreName();
+		foreach ($arrStoreName as $strStoreName){
+			$arrShoppingList[$arrStoreName] = $this->SelectShoppingList($arrList, $strStoreName);
+		}
+		//foreach ($arrShopping["fltPrice"])
+	}*/
 }
 
 ?>
